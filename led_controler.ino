08dd03a741f24ff82ae90bb6bbe9b66a4bb078c8
SolanaFish <michaql1@gmail.com>
2016-10-10 15:42:40 +0200

    implemented encoders, not tested

--- led_controler.ino
+++ led_controler.ino
@@ -13,11 +13,24 @@
 #define encoder1Pin2 4
 #define encoder2Pin1 7
 #define encoder2Pin2 8
+#define encoder3Pin1 9
+#define encoder3Pin2 10
 
 // LED current color
 unsigned short redLed = 255;
 unsigned short greenLed = 255;
 unsigned short blueLed = 255;
+
+// Encoder variables
+  // Encoder 1
+  short encoder1Last = low;
+  short encoder1New = low;
+  // Encoder 2
+  short encoder2Last = low;
+  short encoder2New = low;
+  // Encoder 3
+  short encoder3Last = low;
+  short encoder3New = low;
 
 // LED current mode
 // 0 - color
@@ -39,10 +52,47 @@ void setup() {
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
+
+  // pin setup for ENCODERs
+  pinMode(encoder1Pin1, INPUT);
+  pinMode(encoder1Pin2, INPUT);
+  pinMode(encoder2Pin1, INPUT);
+  pinMode(encoder2Pin2, INPUT);
+  pinMode(encoder3Pin1, INPUT);
+  pinMode(encoder3Pin2, INPUT);
+
 }
 
 void loop() {
-  switch(ledMode) {
+  encoder1New = digitalRead(encoder1Pin1);
+  if(encoder1Last == LOW && encoder1New == HIGH) {
+    if(digitalRead(encoder1Pin2) == LOW) {
+      // rotated back
+    } else {
+      // rotated forward
+    }
+    encoder1Last = encoder1New;
+    continue; //TODO: that works ? 
+  }
+  encoder2New = digitalRead(encoder2Pin1);
+  if(encoder2Last == LOW && encoder2New == HIGH) {
+    if(digitalRead(encoder2Pin2) == LOW) {
+      // rotated back
+    } else {
+      // rotated forward
+    }
+    encoder2Last = encoder2New;
+  }
+  encoder3New = digitalRead(encoder3Pin1);
+  if(encoder3Last == LOW && encoder3New == HIGH) {
+    if(digitalRead(encoder3Pin2) == LOW) {
+      // rotated back
+    } else {
+      // rotated forward
+    }
+    encoder3Last = encoder3New;
+  }
+  switch(ledMode) { // LED controlls
     case 1: { // 1 - breathing
       ledBrightnes += brightnesStep;
       if(brightnesStep > 0) {
@@ -59,7 +109,7 @@ void loop() {
       analogWrite(blueLedPin, blueLed*ledBrightnes);
     } break;
     case 2: { // 2 - rainbow
-      
+
     }
   }
 //  delay(30);

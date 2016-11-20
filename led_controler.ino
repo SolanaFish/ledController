8f00b2a22e9d6a393fa00854c363f345132a3552
SolanaFish <michaql1@gmail.com>
2016-11-20 19:36:41 +0100

    Debounced buttons, implemented indicator leds

--- led_controler.ino
+++ led_controler.ino
@@ -1,5 +1,5 @@
 // LED PINS
-#define redLedPin 3
+#define redLedPin 9
 #define greenLedPin 5
 #define blueLedPin 6
 
@@ -10,7 +10,7 @@
 
 // BUTTON PINS
 #define buttonPin2 14
-#define buttonPin3 15
+// #define buttonPin3 15
 #define buttonPin1 16
 
 // ENCODER PINS
@@ -18,8 +18,15 @@
 #define encoder1Pin2 4
 #define encoder2Pin1 7
 #define encoder2Pin2 8
-#define encoder3Pin1 9
+#define encoder3Pin1 3
 #define encoder3Pin2 10
+
+// RGB INDICATOR PINS
+#define INDICATOR_POWER 15
+#define INDICATOR_R A0
+#define INDICATOR_G A1
+#define INDICATOR_B A2
+
 
 // LED current color
 unsigned short redLed = 255;
@@ -46,7 +53,7 @@ unsigned short ledMode = MODECOLOR;
 
 // LED brightnes
 float ledBrightnes = 1;
-float brightnesStep = -0.05;
+float brightnesStep = -0.001;
 float brightnesStepManual = 0.05;
 unsigned short ledColorStepManual = 10;
 unsigned short encoder1Color = RED;
@@ -60,7 +67,7 @@ void setup() {
   // pin setup for BUTTONs
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
+  // pinMode(buttonPin3, INPUT);
 
   // pin setup for ENCODERs
   pinMode(encoder1Pin1, INPUT);
@@ -69,6 +76,24 @@ void setup() {
   pinMode(encoder2Pin2, INPUT);
   pinMode(encoder3Pin1, INPUT);
   pinMode(encoder3Pin2, INPUT);
+
+  // pin setup for INDICATOR led
+  pinMode(INDICATOR_POWER, OUTPUT);
+  pinMode(INDICATOR_R, OUTPUT);
+  pinMode(INDICATOR_G, OUTPUT);
+  pinMode(INDICATOR_B, OUTPUT);
+  digitalWrite(INDICATOR_POWER, HIGH);
+  // Set red as a default color for INDICATOR led
+  digitalWrite(INDICATOR_R, LOW);
+  digitalWrite(INDICATOR_G, HIGH);
+  digitalWrite(INDICATOR_B, HIGH);
+
+  // lightup leds in default color
+  analogWrite(redLedPin, redLed*ledBrightnes);
+  analogWrite(greenLedPin, greenLed*ledBrightnes);
+  analogWrite(blueLedPin, blueLed*ledBrightnes);
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -147,49 +172,82 @@ void loop() {
   }
 
   // Volume control
-  if(encoder3New == LOW) {
-      while(digitalRead(encoder3Pin1) == LOW){
-      }
-      if(digitalRead(encoder3Pin2) == LOW) {
-        // rotated back
-      } else {
-        // rotated forward
-      }
-      return;
-  }
+  // if(encoder3New == LOW) {
+  //     while(digitalRead(encoder3Pin1) == LOW){
+  //     }
+  //     while(true) {
+  //         Serial.println("test");
+  //         delay(50);
+  //     }
+  //     if(digitalRead(encoder3Pin2) == LOW) {
+  //       // rotated back
+  //     } else {
+  //       // rotated forward
+  //     }
+  //     return;
+  // }
 
 ///////////////////////////////////////////////////////////////
 //BUTTON CONTROL
 ///////////////////////////////////////////////////////////////
-  // if(digitalRead(buttonPin1) == HIGH) {
-  //   while (digitalRead(buttonPin1) == HIGH) {
-  //     delay(1); // TODO: try without delay
-  //   }
-  //   // button action
-  //   return;
-  // }
-  //
-  // if(digitalRead(buttonPin2) == HIGH) {
-  //   while (digitalRead(buttonPin2) == HIGH) {
-  //     delay(1); // TODO: try without delay
-  //   }
-  //   if(ledMode == MODERAINBOW) {
-  //       ledMode = MODECOLOR;
-  //   } else {
-  //       ledMode++;
-  //   }
-  //   return;
-  // }
-  //
-  // if(digitalRead(buttonPin1) == HIGH) {
-  //   while (digitalRead(buttonPin2) == HIGH) {
-  //     delay(1); // TODO: try without delay
+
+// Active color
+    if(digitalRead(buttonPin1) == HIGH) {
+        while (digitalRead(buttonPin1) == HIGH) {
+        }
+        if(encoder1Color == BLUE) {
+            encoder1Color = RED;
+        } else {
+            encoder1Color++;
+        }
+        // Change color of indicator led
+        switch(encoder1Color) {
+            case RED: {
+                digitalWrite(INDICATOR_R, LOW);
+                digitalWrite(INDICATOR_G, HIGH);
+                digitalWrite(INDICATOR_B, HIGH);
+            }break;
+            case GREEN: {
+                digitalWrite(INDICATOR_R, HIGH);
+                digitalWrite(INDICATOR_G, LOW);
+                digitalWrite(INDICATOR_B, HIGH);
+            }break;
+            case BLUE: {
+                digitalWrite(INDICATOR_R, HIGH);
+                digitalWrite(INDICATOR_G, HIGH);
+                digitalWrite(INDICATOR_B, LOW);
+            }break;
+        }
+        // delay for debouncing (500 is a lot, and you can't spam button)
+        delay(500);
+        while (digitalRead(buttonPin1) == HIGH) {
+        }
+        return;
+    }
+// LED mode
+  if(digitalRead(buttonPin2) == HIGH) {
+    while (digitalRead(buttonPin2) == HIGH) {
+    }
+    if(ledMode == MODERAINBOW) {
+        ledMode = MODECOLOR;
+    } else {
+        ledMode++;
+    }
+  // delay for debouncing
+  delay(500);
+    return;
+  }
+
+  // if(digitalRead(buttonPin3) == HIGH) {
+  //   while (digitalRead(buttonPin3) == HIGH) {
   //   }
   //   if(encoder1Color == BLUE) {
   //       encoder1Color = RED;
   //   } else {
   //       encoder1Color++;
   //   }
+  // delay for debouncing
+  // delay(500);
   //   return;
   // }
 ///////////////////////////////////////////////////////////////
@@ -211,11 +269,11 @@ void loop() {
                 analogWrite(redLedPin, redLed*ledBrightnes);
                 analogWrite(greenLedPin, greenLed*ledBrightnes);
                 analogWrite(blueLedPin, blueLed*ledBrightnes);
+                delay(10);
             } break;
             case MODERAINBOW: { // 2 - rainbow
 
             }
         }
     }
- // delay(30);
 }
